b1a24b1d2cd0dc9dbdf8415a13dd303be711a89e
nihole <nihole@mail.ru>
2017-05-31 01:11:21 +0200

    CONFD added

---
+++ CONFG/psefabric-conf.c
@@ -0,0 +1,161 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/poll.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <memory.h>
+#include <confd_lib.h>
+#include <confd_cdb.h>
+
+/* include generated file */
+#include "policy.h"
+
+
+char* execute(const char* cmd)
+{
+
+  FILE *fp;
+  char buffer[128];
+  char* result = "";
+
+  /* Open the command for reading. */
+  fp = popen(cmd, "r");
+  if (fp == NULL) {
+    printf("Failed to run command\n" );
+    exit(1);
+  }
+
+  /* Read the output a line at a time - output it. */
+  while (fgets(buffer, sizeof(buffer)-1, fp) != NULL) {
+    char* result_new  = (char*) malloc(128 + strlen(result));
+    strcpy(result_new, result);
+    strcat(result_new, buffer);
+    result = result_new;
+  }
+
+
+  /* close */
+  pclose(fp);
+
+  return result;
+}
+
+static int read_conf (struct sockaddr_in *addr) {
+    int rsock;
+    char* res ="";
+    if ((rsock = socket(PF_INET, SOCK_STREAM, 0)) < 0 )
+        confd_fatal("Failed to open socket\n");
+
+    if (cdb_connect(rsock, CDB_READ_SOCKET, (struct sockaddr*)addr,
+                      sizeof (struct sockaddr_in)) < 0)
+        return CONFD_ERR;
+    if (cdb_start_session(rsock, CDB_RUNNING) != CONFD_OK)
+        return CONFD_ERR;
+/*    res = execute("timeout 10 python ./conf-scripts/get-xml-diff.py 2>/dev/null"); */  
+    res = execute("timeout 10 python ../PSEF_SCRIPTS/psefabric.py");               
+
+    if (strcmp(res,"OK\n") != 0) {
+        printf ("%s", res);
+        cdb_close(rsock);              
+        return 0;
+    }
+    else {
+        printf ("%s", res);
+        return cdb_close(rsock);
+   }
+}
+
+
+/********************************************************************/
+
+int main(int argc, char **argv)
+{
+    struct sockaddr_in addr;
+    int subsock;
+    int status;
+    int spoint;
+
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(CONFD_PORT);
+
+    confd_init(argv[0], stderr, CONFD_TRACE);
+
+    /*
+     * Setup subscriptions
+     */
+    if ((subsock = socket(PF_INET, SOCK_STREAM, 0)) < 0 )
+        confd_fatal("Failed to open socket\n");
+
+    if (cdb_connect(subsock, CDB_SUBSCRIPTION_SOCKET, (struct sockaddr*)&addr,
+                      sizeof (struct sockaddr_in)) < 0)
+        confd_fatal("Failed to cdb_connect() to confd \n");
+
+    if ((status = cdb_subscribe(subsock, 3, policy__ns, &spoint, "/"))
+        != CONFD_OK) {
+        fprintf(stderr, "Terminate: subscribe %d\n", status);
+        exit(0);
+    }
+    if (cdb_subscribe_done(subsock) != CONFD_OK)
+        confd_fatal("cdb_subscribe_done() failed");
+    printf("Subscription point = %d\n", spoint);
+
+    /*
+     * Read initial config
+     */
+    if ((status = read_conf(&addr)) != CONFD_OK) {
+        fprintf(stderr, "Terminate: read_conf %d\n", status);
+        exit(1);
+    }
+    rename("policy.conf.tmp", "policy.conf");
+    /* This is the place to HUP the daemon */
+
+    while (1) {
+        static int poll_fail_counter=0;
+        struct pollfd set[1];
+
+        set[0].fd = subsock;
+        set[0].events = POLLIN;
+        set[0].revents = 0;
+
+        if (poll(&set[0], 1, -1) < 0) {
+            perror("Poll failed:");
+            if(++poll_fail_counter < 10)
+                continue;
+            fprintf(stderr, "Too many poll failures, terminating\n");
+            exit(1);
+        }
+
+        poll_fail_counter = 0;
+        if (set[0].revents & POLLIN) {
+            int sub_points[1];
+            int reslen;
+
+            if ((status = cdb_read_subscription_socket(subsock,
+                                                       &sub_points[0],
+                                                       &reslen)) != CONFD_OK) {
+                fprintf(stderr, "terminate sub_read: %d\n", status);
+                exit(1);
+            }
+            if (reslen > 0) {
+                if ((status = read_conf(&addr)) != CONFD_OK) {
+                    fprintf(stderr, "Terminate: read_conf %d\n", status);
+                    exit(1);
+                }
+            }
+            fprintf(stderr, "Read new config, updating dhcpd config \n");
+            rename("policy.conf.tmp", "policy.conf");
+            /* this is the place to HUP the daemon */
+
+            if ((status = cdb_sync_subscription_socket(subsock,
+                                                       CDB_DONE_PRIORITY))
+                != CONFD_OK) {
+                fprintf(stderr, "failed to sync subscription: %d\n", status);
+                exit(1);
+            }
+        }
+    }
+}
