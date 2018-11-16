9803844a1406855ecfc2b4677f149778a523229f
root <nihole>
2018-11-16 18:50:20 +0000

    acitemplates create/delete verified

--- PROJECTS/p002/PSEF_SCRIPTS/acitemplates.py
+++ PROJECTS/p002/PSEF_SCRIPTS/acitemplates.py
@@ -4,34 +4,39 @@ import vocabulary
 
 def aci_attach_epg (contract_name, epg_name, path, relation):
     t = re.search('(uni/.*)/.*$', path)
-    a = re.search('uni/.*/(.*)$', path)
+    a = re.search('uni/.*/ap-(.*)$', path)
     tenant_path = t.group(1)
     app = a.group(1)
-    epg_path = path + '/' + epg_name 
+    epg_path = path + '/' + 'epg-' + epg_name 
 
 
     if (relation == 'cons'):
+        attribute = "fvRsCons"
         contract_path = epg_path + '/' + 'rscons-%s' % contract_name
     if (relation == 'prov'):
+        attribute = "fvRsProv"
         contract_path = epg_path + '/' + 'rsprov-%s' % contract_name
 
-    config_json = '{"fvTenant":{"attributes":{"dn":%s,"status":"modified"},"children":[{"fvAp":{"attributes":{"dn":%s,"name":%s, "status":"modified"},"children":[{"fvAEPg":{"attributes":{"dn":%s,"status":"modified"},"children":[{"fvRsCons":{"attributes":{"dn":%s,"status":"created,modified"},"children":[]}}]}}]}}]}}' % (tenant_path,path,app,epg_path,contract_path)
+    config_json = '{"fvTenant":{"attributes":{"dn":"%s","status":"modified"},"children":[{"fvAp":{"attributes":{"dn":"%s","name":"%s", "status":"modified"},"children":[{"fvAEPg":{"attributes":{"dn":"%s","status":"modified"},"children":[{"%s":{"attributes":{"dn":"%s","status":"created,modified"},"children":[]}}]}}]}}]}}' % (tenant_path,path,app,epg_path, attribute, contract_path)
     
     return (config_json)
 
 def aci_detach_epg (contract_name, epg_name, path, relation):
 
     t = re.search('(uni/.*)/.*$', path)
-    a = re.search('uni/.*/(.*)$', path)
+    a = re.search('uni/.*/ap-(.*)$', path)
     tenant_path = t.group(1)
     app = a.group(1)
-    epg_path = path + '/' + epg_name
+    epg_path = path + '/' + 'epg-' + epg_name
     if (relation == 'cons'):
+        attribute = "fvRsCons"
         contract_path = epg_path + '/' + 'rscons-%s' % contract_name
     if (relation == 'prov'):
+        attribute = "fvRsProv"
         contract_path = epg_path + '/' + 'rsprov-%s' % contract_name
 
-    config_json = '{"fvTenant":{"attributes":{"dn":%s,"status":"modified"},"children":[{"fvAp":{"attributes":{"dn":%s,"name":%s, "status":"deleted"},"children":[{"fvAEPg":{"attributes":{"dn":%s,"status":"modified"},"children":[{"fvRsCons":{"attributes":{"dn":%s,"status":"created,modified"},"children":[]}}]}}]}}]}}' % (tenant_path,path,app,epg_path,contract_path)
+    config_json = '{"fvTenant":{"attributes":{"dn":"%s","status":"modified"},"children":[{"fvAp":{"attributes":{"dn":"%s","name":"%s", "status":"modified"},"children":[{"fvAEPg":{"attributes":{"dn":"%s","status":"modified"},"children":[{"%s":{"attributes":{"dn":"%s","status":"deleted"},"children":[]}}]}}]}}]}}' % (tenant_path,path,app,epg_path, attribute, contract_path)
+
 
 
     return (config_json)
@@ -53,7 +58,7 @@ def aci_create_policy (eq_parameter, psefname, epg_dict_cons, epg_dict_prov, app
     for subject_el in subject_dict:
         config_fltr_ = ''
         for filter_el in subject_el['services']:
-            config_fltr_el = '{"vzRsSubjFiltAtt":{"attributes":{"action":"permit","priorityOverride":"default","tnVzFilterName":"%s"}}}' % filter_el['parameters'][vocabulary.par_rvoc['aci-filter-name']] 
+            config_fltr_el = '{"vzRsSubjFiltAtt":{"attributes":{"tnVzFilterName":"%s"}}}' % filter_el['parameters'][vocabulary.par_rvoc['aci-filter-name']] 
             if (config_fltr_ == ''):
                 config_fltr_ = config_fltr_el
             else:
@@ -68,7 +73,7 @@ def aci_create_policy (eq_parameter, psefname, epg_dict_cons, epg_dict_prov, app
         config_sbj = '[' + config_sbj + '}}]'
 
 # Contracts creation:    
-    config_cntr_part = '{"fvTenant":{"attributes":{"dn":"uni/tn-common","status":"modified"},"children":[{"vzBrCP":{"attributes":{"dn":"uni/tn-common/brc-%s","name":"%s","scope":"context"},"children":' % (subject_el['parameters'][vocabulary.par_rvoc['aci-subject-name'] ], subject_el['parameters'][vocabulary.par_rvoc['aci-subject-name'] ])
+    config_cntr_part = '{"fvTenant":{"attributes":{"dn":"uni/tn-common","status":"modified"},"children":[{"vzBrCP":{"attributes":{"dn":"uni/tn-common/brc-%s","name":"%s","scope":"context"},"children":' % (name, name)
     config_cntr = config_cntr_part + config_sbj + '}}]}}'
 
 # EPGs attachment
